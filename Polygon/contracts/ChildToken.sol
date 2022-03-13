ecc23ebf9488667b7b61925b9d4450b7053c158e
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2022-03-12 19:45:01 -0500

    started polygon implementation

---
+++ Polygon/contracts/ChildToken.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IFxERC20} from "./fxPortal/IFxERC20.sol";
+
+
+contract ChildToken is IFxERC20, ERC20 {
+    address internal _fxManager;
+    address internal _connectedToken;
+
+    constructor(address fxManager_, address connectedToken_) ERC20("LayerTwo Token", "LTWO") {
+         _fxManager = fxManager_;
+        _connectedToken = connectedToken_;
+    }
+    function initialize(
+        address fxManager_,
+        address connectedToken_,
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_
+    ) public override {
+        require(_fxManager == address(0x0) && _connectedToken == address(0x0), "Token is already initialized");
+        _fxManager = fxManager_;
+        _connectedToken = connectedToken_;
+
+    }
+
+    // fxManager rturns fx manager
+    function fxManager() public view override returns (address) {
+        return _fxManager;
+    }
+
+    // connectedToken returns root token
+    function connectedToken() public view override returns (address) {
+        return _connectedToken;
+    }
+
+
+    function mint(address user, uint256 amount) public override {
+        require(msg.sender == _fxManager, "Invalid sender");
+        _mint(user, amount);
+    }
+
+    function burn(address user, uint256 amount) public override {
+        require(msg.sender == _fxManager, "Invalid sender");
+        _burn(user, amount);
+    }
+}
