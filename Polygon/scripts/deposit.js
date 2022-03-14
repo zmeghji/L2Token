ace1708aa96aaa5d9074366177cc0fe3a03d8fab
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2022-03-14 19:06:57 -0400

    completed deposit script and started withdrawal

---
+++ Polygon/scripts/deposit.js
@@ -0,0 +1,32 @@
+const hre = require("hardhat");
+
+
+async function main(){
+    let rootTokenAddress = "0x5A9C69ce46F770AAD768604Ac382873133C094c3";
+    let rootTunnelAddress = "0x28a717D0419c4207bac8aCB658Ad6B90124A5044";
+
+    let amount = hre.ethers.utils.parseEther('1000.0');
+
+    const rootToken = await hre.ethers.getContractAt("RootToken", rootTokenAddress);
+
+    //approve the rootTunnel with the amount of ERC 20 tokens
+    console.log("Approve the root tunnel with the amount of ERC20 tokens");
+    await rootToken.approve(rootTunnelAddress, amount);
+    await new Promise(resolve => setTimeout(resolve, 20000));
+
+
+    const deployer = (await hre.ethers.getSigners())[0];
+    const rootTunnel = await hre.ethers.getContractAt("FxERC20RootTunnel", rootTunnelAddress);
+
+    //deposit tokens to polygon chain
+    console.log ("deposit tokens to polygon from root tunnel")
+    await rootTunnel.deposit(rootTokenAddress, deployer.address, amount, hre.ethers.utils.formatBytes32String(""));
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ Polygon/scripts/withdraw1.js
@@ -0,0 +1,35 @@
+const hre = require("hardhat");
+
+
+async function main(){
+    
+    let childTokenAddress = "0xeA3503a712E3af7bb01800B99fC28Cf7721c53e4";
+    let childTunnelAddress = "0x5A9C69ce46F770AAD768604Ac382873133C094c3";
+
+    let amount = hre.ethers.utils.parseEther('1000.0');
+
+    const childToken = await hre.ethers.getContractAt("ChildToken", childTokenAddress);
+
+    //approve the childTunnel with the amount of ERC 20 tokens
+    console.log("Approve the child tunnel with the amount of ERC20 tokens");
+    await childToken.approve(childTunnelAddress, amount);
+    await new Promise(resolve => setTimeout(resolve, 20000));
+
+
+    const childTunnel = await hre.ethers.getContractAt("FxERC20ChildTunnel", childTunnelAddress);
+
+    //deposit tokens to polygon chain
+    console.log ("withdraw tokens from polygon from root tunnel")
+    let tx = await childTunnel.withdraw(childTokenAddress, amount);
+    console.log("transaction: " + tx);
+    console.log("transaction hash: " + tx.hash);
+
+    //0x7f4c900cc61e5391fefead925350874f9fe415126761cf1b31bd934e2ed88510
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
